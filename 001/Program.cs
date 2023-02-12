f0c6c5ad51191589e34949126098acb3a1a0cbb8
Aleksey Kanaev <my@akanaev.ru>
2023-02-12 11:17:31 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Квадрат числа " + value + " равен: " + value * value);
+
+Console.WriteLine("Способо 2. Квадрат числа " + value + " равен: " + Math.Pow(value, 2));
