541375460b5b6142a88fce84a35ea6bd50d49823
Aleksey Kanaev <my@akanaev.ru>
2023-02-18 22:52:07 +0300

    домашка

---
+++ Домашка/Homework08/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Clear();
+Console.Write("Write number 1: ");
+int N=int.Parse(Console.ReadLine());
+Console.Write("Write number 2: ");
+int m = int.Parse(Console.ReadLine());
+while (int i = n; i <= m; i++)
+{
+    Console.WriteLine($"{B} ");
+    B++;
+}
