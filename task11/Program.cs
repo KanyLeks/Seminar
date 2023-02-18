17f58520b68d6d79dcc01c16c37851ce1117ddac
Aleksey Kanaev <my@akanaev.ru>
2023-02-18 19:20:16 +0300

    Координаты, используя switch

--- task11/Program.cs
+++ task11/Program.cs
@@ -1,51 +1,58 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, 
 // показывает диапазон возможных координат точек в этой четверти (x и y)
 
-Console.Clear();
+// Console.Clear();
 
+// Console.WriteLine("Enter Number Quarter:");
+
+// int num = int.Parse(Console.ReadLine());
+
+// if (num == 1)
+// {
+//     Console.WriteLine("(X > 0 , Y > 0)");
+// }
+
+// if (num == 2)
+// {
+//     Console.WriteLine("(X < 0 , Y > 0)");
+// }
+// if (num == 3)
+// {
+//     Console.WriteLine("(X < 0 , Y < 0)");
+// }
+
+// if (num == 4)
+// {
+//     Console.WriteLine("(X > 0 , Y < 0)");
+// }
+// else
+// {
+//     Console.WriteLine("quarter does not exist");
+// }
+
+
+// 2 Способ, используя switch.
 Console.WriteLine("Enter Number Quarter:");
-
 int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.WriteLine("(X > 0 , Y > 0)");
+    break;
 
-if (num ==1)
-{
-    Console.WriteLine("(X > 0 , Y > 0)");
-}
 
-if (num ==2)
-{
-Console.WriteLine("(X < 0 , Y > 0)");
-}
-if (num ==3)
-{
-Console.WriteLine("(X < 0 , Y < 0)");
-}
+    case 2:
+        Console.WriteLine("(X < 0 , Y > 0)");
+    break;
 
-if (num ==4)
-{
-Console.WriteLine("(X > 0 , Y < 0)");
-}
-else 
-{
-    Console.WriteLine("Quarter undefined");
-}
-// if (X > 0 && Y > 0)
-// {
-//     Console.WriteLine("1 quarter");
-// }
-// else if (X < 0 && Y > 0)
-// {
-//     Console.WriteLine("2 quarter");
-// }
-// else if (X < 0 && Y < 0)
-// {
-//     Console.WriteLine("3 quarter");
-// }
-// else if (X > 0 && Y < 0)
-// {
-//     Console.WriteLine("4 quarter");
-// }
-// int X = int.Parse(Console.ReadLine());
+    case 3:
+        Console.WriteLine("(X < 0 , Y < 0)");
+    break;
 
-// Console.WriteLine("Enter Y:");
-// int Y = int.Parse(Console.ReadLine());
+    case 4:
+        Console.WriteLine("(X > 0 , Y < 0)");
+    break;
+
+    default : Console.WriteLine ("Quarter undefined");
+    break;
+}
