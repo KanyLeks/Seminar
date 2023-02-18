d5545fc0572e2b1ad00f2096b1c695bd5146f043
Aleksey Kanaev <my@akanaev.ru>
2023-02-18 21:20:02 +0300

    Выдает квадрат чисел

---
+++ task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N)  и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2-> 1, 4.
+
+
+Console.Clear();
+
+void WriteConsolePowTableN(int value)
+{
+    for (int i = 1; i <= value; i++)
+    {
+        Console.Write($"{Math.Pow(i, 2)}");
+        if (i < value)
+        {
+            Console.Write($", ");
+        }
+    }
+    Console.WriteLine(".");
+}
+
+WriteConsolePowTableN(5);
+
+WriteConsolePowTableN(2);
+
+
