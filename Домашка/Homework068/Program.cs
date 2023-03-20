dd49db066b391afc670c4fc388963c8fb7430541
Aleksey Kanaev <my@akanaev.ru>
2023-03-20 20:15:56 +0300

    Евклит

---
+++ Домашка/Homework068/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+M = 28; N = 7 -> 7
+*/
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Наибольший общий делитель чисел {0} и {1} равен {2}", m, n, LeastCommonDivisor(m, n));
+
+
+int LeastCommonDivisor(int m, int n)
+{
+    while (n != 0)
+    {
+        int temp = n;
+        n = m % n;
+        m = temp;
+    }
+    return m;
+}
